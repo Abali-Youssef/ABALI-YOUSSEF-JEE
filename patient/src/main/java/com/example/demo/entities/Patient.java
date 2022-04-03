b51ffffe0b4f76bf83f86b69f1431fff9e3ce5f3
Abali-Youssef <youssabali486@gmail.com>
2022-04-03 14:00:46 +0000

    adding patient form + template

--- patient/src/main/java/com/example/demo/entities/Patient.java
+++ patient/src/main/java/com/example/demo/entities/Patient.java
@@ -3,6 +3,7 @@ package com.example.demo.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -16,8 +17,10 @@ public class Patient {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "YYYY-MM-dd")
     private Date dateNaissance;
     private boolean malade;
     private int score;
--- patient/src/main/java/com/example/demo/web/PatientController.java
+++ patient/src/main/java/com/example/demo/web/PatientController.java
@@ -11,30 +11,46 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
-@Data @AllArgsConstructor @NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientController {
     @Autowired
     private PatientRepository patientRepository;
-    @GetMapping(path  = "/index")
-    public String patients (Model model,
-                            @RequestParam(name = "page", defaultValue = "0") int page,
-                            @RequestParam (name = "size", defaultValue = "4") int size,
-                            @RequestParam (name = "key", defaultValue = "") String key){
-        Page<Patient> pagePatients=patientRepository.findByNomLike("%"+key+"%",PageRequest.of(page, size));
-        model.addAttribute(  "listPatients", pagePatients.getContent ());
-        model.addAttribute( "pages", new int[pagePatients.getTotalPages ()]);
-        model.addAttribute(  "currentPage", page);
-        model.addAttribute(  "key", key);
+
+    @GetMapping(path = "/index")
+    public String patients(Model model,
+                           @RequestParam(name = "page", defaultValue = "0") int page,
+                           @RequestParam(name = "size", defaultValue = "4") int size,
+                           @RequestParam(name = "key", defaultValue = "") String key) {
+        Page<Patient> pagePatients = patientRepository.findByNomLike("%" + key + "%", PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("key", key);
         return "patient";
     }
+
     @GetMapping("/delete")
-    public String delete(Long id,String key,int page){
+    public String delete(Long id, String key, int page) {
         patientRepository.deleteById(id);
-        return "redirect:/index?page="+page+"&key="+key;
+        return "redirect:/index?page=" + page + "&key=" + key;
+    }
+
+    @GetMapping("/formpatient")
+    public String formpatient(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "formpatients";
+    }
+    @PostMapping("/save")
+    public String save(Model model,Patient patient) {
+        patientRepository.save(patient);
+        return "formpatients";
     }
 }
