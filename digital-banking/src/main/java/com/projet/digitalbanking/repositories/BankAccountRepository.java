b5944b72680bb6b3c7eea3002ccda03dece72ae9
Abali-Youssef <youssabali486@gmail.com>
2022-05-22 12:02:16 +0100

    entities + repositories + dependencies

---
+++ digital-banking/src/main/java/com/projet/digitalbanking/repositories/BankAccountRepository.java
@@ -0,0 +1,7 @@
+package com.projet.digitalbanking.repositories;
+
+import com.projet.digitalbanking.entities.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+}
